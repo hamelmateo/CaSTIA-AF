e3a1ed8ab5fdcfbc1de44ac45ae7e8f9124a19e3
hamelmateo <hamel.mateo.fr@gmail.com>
2025-04-03 11:41:33 -0400

    folder menu

--- src/gui/__main__.py
+++ src/gui/__main__.py
@@ -1,12 +1,18 @@
 # python -m src.gui
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QFileDialog
 import sys
 from src.gui.viewer import OverlayViewer
-
+from pathlib import Path
 
 def main():
     app = QApplication(sys.argv)
-    viewer = OverlayViewer()
+
+    folder = QFileDialog.getExistingDirectory(None, "Select Analysis Folder")
+    if not folder:
+        print("[INFO] No folder selected. Exiting.")
+        return
+
+    viewer = OverlayViewer(Path(folder))
     viewer.show()
     sys.exit(app.exec_())
 
